0963bec04f897b59ed7e3385148f7f81aaae3ecc
Emiel van der Meijs <epicpinguin33@gmail.com>
2019-05-24 13:39:45 +0200

    green light presentation

--- function generators/getDelay.m
+++ function generators/getDelay.m
@@ -1,5 +1,5 @@
 function [r] = getDelay(R,sampleRate)
 %GETDELAY NS simulator
 attenuation=R^(-2);
-r=systemBlock([zeros(1,R*sampleRate) attenuation]);
+r=systemBlock([zeros(1,floor(R*sampleRate/299792458)) attenuation]);
 end
